7169064dc48119c85fda5f767262499a6400630d
zhang <75647651@qq.com>
2026-05-08 00:50:09 +0800

    MVP0

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ import os
 from dotenv import load_dotenv
 
 # 加载 .env 文件（从项目根目录）
-env_path = os.path.join(os.path.dirname(__file__), "..", ".env")
+env_path = os.path.join(os.path.dirname(__file__), "", ".env")
 load_dotenv(env_path)
 
 
